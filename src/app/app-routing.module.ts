f9eb5adda4f1c68d761e8207acef32a845c485ed
Oliver Lance <42589078+ollylance@users.noreply.github.com>
2023-09-28 14:43:20 -0700

    remove portfolio routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { MontavieComponent } from './projects/montavie/montavie.component';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/portfolio', pathMatch: 'full' },
-  { path: 'portfolio', component: HomeComponent },
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
